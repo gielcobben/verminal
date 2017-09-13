280e521fea5c6d702acfd466491847bf0e6fd56e
Vernon de Goede <vernon@ecurring.nl>
2017-09-13 11:09:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+module.exports.onWindow = browserWindow => browserWindow.setVibrancy("dark");
+
+const foregroundColor = "#fff";
+const backgroundColor = "rgba(0, 0, 0, .60)";
+const overlap = "rgba(0, 0, 0, .15)";
+const red = "#FF3B30";
+const green = "#4CD964";
+const yellow = "#FFCC00";
+const blue = "#0095FF";
+const magenta = "#FF2D55";
+const cyan = "#5AC8FA";
+const white = "#FFFFFF";
+
+exports.decorateConfig = config =>
+  Object.assign({}, config, {
+    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontSize: 12,
+    backgroundColor,
+    foregroundColor,
+    borderColor: overlap,
+    cursorColor: blue,
+    colors: {
+      black: backgroundColor,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white,
+      lightBlack: "#686868",
+      lightRed: red,
+      lightGreen: green,
+      lightYellow: yellow,
+      lightBlue: blue,
+      lightMagenta: magenta,
+      lightCyan: cyan,
+      lightWhite: foregroundColor
+    },
+    css: `
+    ${config.css}
+    .hyper_main {
+      border: none !important;
+    }
+    .header_header {
+      background-color: ${overlap} !important;
+    }
+    .tabs_borderShim {
+      border-color: transparent !important;
+    }
+    .tab_tab {
+      border: 0;
+    }
+    .tab_textActive {
+      border-bottom: 2px solid ${green};
+    }
+  `
+  });
